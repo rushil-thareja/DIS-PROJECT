2eb1da0bed2756e3c601bf1d7d90ceeee2146b87
rushil-thareja <rushilthareja@gmail.com>
2020-11-21 16:40:48 +0530

    FIRST

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.Material;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+    private ArFragment arFragment;
+    private ModelRenderable modelRenderable;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        setUpModel();
+        setUpPlane();
+    }
+    private void setUpModel(){
+        ModelRenderable.builder()
+                .setSource(this,R.raw.hamburger)
+                .build()
+                .thenAccept(renderable->modelRenderable = renderable)
+                .exceptionally(throwable -> {
+                    Toast.makeText(MainActivity.this,"not loaded",Toast.LENGTH_LONG).show();
+                    return(null);
+                });
+
+    }
+    private void setUpPlane(){
+        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                Anchor anchor = hitResult.createAnchor();
+                AnchorNode anchorNode = new AnchorNode(anchor);
+                anchorNode.setParent(arFragment.getArSceneView().getScene());
+                createModel(anchorNode);
+            }
+        });
+    }
+    private void createModel(AnchorNode anchorNode){
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.setParent(anchorNode);
+        node.setRenderable(modelRenderable);
+        node.select();
+    }
+}
